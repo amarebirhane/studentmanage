4862067478ecde17437eacdbbb9b8adcc875f425
amarebirhane <amarebirhane89@gmail.com>
2026-02-01 20:36:49 +0300

    updated some

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,7 @@ import authRoutes from './modules/auth/auth.routes';
 import studentRoutes from './modules/students/student.routes';
 import classRoutes from './modules/classes/class.routes';
 import userRoutes from './modules/users/user.routes';
+import teacherRoutes from './modules/teachers/teacher.routes';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use('/auth', authRoutes);
 router.use('/students', studentRoutes);
 router.use('/classes', classRoutes);
 router.use('/admin', userRoutes);
+router.use('/teachers', teacherRoutes);
 
 export default router;

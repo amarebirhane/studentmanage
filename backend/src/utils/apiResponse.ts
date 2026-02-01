5ec90a25171f20f5de05aace5c468db98460dc04
amarebirhane <amarebirhane89@gmail.com>
2026-02-01 21:43:29 +0300

    updated the some error

--- backend/src/utils/apiResponse.ts
+++ backend/src/utils/apiResponse.ts
@@ -4,7 +4,7 @@ export class ApiError extends Error {
     statusCode: number;
     errors?: any;
 
-    constructor(statusCode: number, message: string, errors: any) {
+    constructor(statusCode: number, message: string, errors: any = null) {
         super(message);
         this.statusCode = statusCode;
         this.errors = errors;

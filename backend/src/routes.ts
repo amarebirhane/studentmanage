35ed7251f56bb92bbf64b3bae5e6d906b94250b1
amarebirhane <amarebirhane89@gmail.com>
2026-02-02 12:09:20 +0300

    added parents portals

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import studentRoutes from './modules/students/student.routes';
 import classRoutes from './modules/classes/class.routes';
 import userRoutes from './modules/users/user.routes';
 import teacherRoutes from './modules/teachers/teacher.routes';
+import parentRoutes from './modules/parents/parent.routes';
 import schoolRoutes from './modules/schools/school.routes';
 import attendanceRoutes from './modules/attendance/attendance.routes';
 import examRoutes from './modules/exams/exam.routes';
@@ -18,6 +19,7 @@ router.use('/students', studentRoutes);
 router.use('/classes', classRoutes);
 router.use('/admin', userRoutes);
 router.use('/teachers', teacherRoutes);
+router.use('/parents', parentRoutes);
 
 // New Routes
 router.use('/schools', schoolRoutes);
